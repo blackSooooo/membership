d0cb6cab37410ffd15e4a8f03ca9d8097a1a4f59
blackSooooo <blacksooooo@naver.com>
2022-12-25 22:21:23 +0900

    feat: Membership 서비스 레이어 추가

---
+++ src/main/kotlin/blacksooooo/membership/domain/MembershipService.kt
@@ -0,0 +1,34 @@
+package blacksooooo.membership.domain
+
+import blacksooooo.membership.common.MembershipErrorResult
+import blacksooooo.membership.common.MembershipType
+import blacksooooo.membership.exception.MembershipException
+import blacksooooo.membership.response.MembershipResponseDto
+import blacksooooo.membership.storage.Membership
+import blacksooooo.membership.storage.MembershipRepository
+import org.springframework.stereotype.Service
+
+@Service
+class MembershipService(
+    private val membershipRepository: MembershipRepository
+) {
+    fun addMembership(
+        userId: String,
+        membershipType: MembershipType,
+        point: Int
+    ): MembershipResponseDto {
+        val result = membershipRepository.findByUserIdAndMembershipType(userId, membershipType)
+        if (result != null) {
+            throw MembershipException(MembershipErrorResult.DUPLICATED_MEMBERSHIP_REGISTER)
+        }
+        val savedMembership = membershipRepository.save(Membership(
+            userId = userId,
+            membershipType = membershipType,
+            point = point
+        ))
+        return MembershipResponseDto(
+            id = savedMembership.id!!,
+            membershipType = savedMembership.membershipType
+        )
+    }
+}
